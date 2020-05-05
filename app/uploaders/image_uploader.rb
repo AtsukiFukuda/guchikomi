17732deace8dcd003c709f93c52bcb518201bc4f
Atsuki <you@example.com>
2020-05-06 00:16:39 +0900

    iroiroを修

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,7 +42,7 @@ end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png heic mp4)
+    %w(jpg jpeg gif png heic)
   end
 
   # Override the filename of the uploaded files:
